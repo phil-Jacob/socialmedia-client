4d87f5e332de81a91f8f33065f1516ab6dfc5867
Phil Jacob <jack.joe1817@gmail.com>
2024-03-23 21:14:10 +0530

    socia

--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          GeekGrid
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
